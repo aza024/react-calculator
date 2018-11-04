5d16927998d8ff51685b5e9db648a876e41273f0
Andrea  Piazza <apiazza023@gmail.com>
2018-11-03 23:39:37 -0700

    imported math.js

--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -5,7 +5,9 @@ const isOperator = val =>{
     return !isNaN(val) || val === "." || val === "="
 }
 export const Button = props => (
-    <div className ={`button-wrapper ${isOperator(props.children) ?  null : "operator"}`} >
+    <div className ={`button-wrapper ${isOperator(props.children) ?  null : "operator"}`} 
+    onClick={()=>props.handleClick(props.children)}
+    >
         {props.children}
     </div>
 )
